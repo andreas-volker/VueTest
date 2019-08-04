06ba4bd77e514fc014de0eae5ca452a75cef9d59
Andreas Völker <andreaskvolker@gmail.com>
2019-08-04 12:31:04 -0300

    build

--- src/components/meuPerfil.vue
+++ src/components/meuPerfil.vue
@@ -1,28 +1,19 @@
 <template>
-  <div class="root">
-    <div class="perfil" v-if="route.path === '/'">
-      perfil
-      <a href="alterar">alterar senha</a>
-    </div>
-    <div v-else>
-      <router-view/>
-    </div>
+  <div class="perfil">
+    perfil
+	  <router-link to="alterar">alterar senha</router-link>
   </div>
 </template>
+
 <script>
 export default {
   name: 'MeuPerfil',
-  data() {
+  data () {
     return {
-      route: {}
     }
-  },
-  created: function() {
-    this.route = this.$route
-    console.log(this.route);
   }
 }
-
 </script>
+
 <style scoped>
 </style>
